d24ae7d87a4b5a58fa341f61d32b0357c404dd97
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2014-09-29 14:45:26 -0700

    Modified the interpolation code to handle scalar values.

---
+++ ShowDampedSlabPlots.m
@@ -0,0 +1,15 @@
+file = '/Volumes/Data/QGPlusSlab/WindForcedFPlane.nc';
+t = ncread(file, 'time');
+x = ncread(file, 'x');
+y = ncread(file, 'y');
+
+u = squeeze(ncread(file, 'u', [1 1 1], [1 1 Inf], [1 1 1]));
+v = squeeze(ncread(file, 'v', [1 1 1], [1 1 Inf], [1 1 1]));
+
+figure,
+plot(t, u, 'b')
+hold on
+plot(t, v, 'r')
+
+h = double(squeeze(ncread(file, 'h', [1 1 70], [Inf Inf 1], [1 1 1])));
+figure, pcolor(x,y,h), shading flat
