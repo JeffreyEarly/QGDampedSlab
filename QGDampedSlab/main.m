fc71fa35ff53eeeb0293a1459862719b59800cd9
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2014-09-29 11:24:06 -0700

    Initial Commit

---
+++ QGDampedSlab/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  QGDampedSlab
+//
+//  Created by Jeffrey J. Early on 9/29/14.
+//  Copyright (c) 2014 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
